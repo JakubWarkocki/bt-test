f3e52505b95cc19ce11118335ec70262eb61c21d
Jakub Warkocki <jakub.warkocki@gmail.com>
2025-03-06 16:44:22 +0100

    twist mux

--- launch/launch.py
+++ launch/launch.py
@@ -12,30 +12,36 @@ def generate_launch_description():
     package_name = 'bt_test'  # Change this to your package name
 
     # Paths to various files
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
+    gazebo_params_file = os.path.join(get_package_share_directory(
+        package_name), 'config', 'gazebo_params.yaml')
     world_file_name = 'basic.world'
-    world_file_path = os.path.join(get_package_share_directory(package_name), 'worlds', world_file_name)
+    world_file_path = os.path.join(get_package_share_directory(
+        package_name), 'worlds', world_file_name)
 
     # Include robot_state_publisher launch file
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+            get_package_share_directory(
+                package_name), 'launch', 'rsp.launch.py'
         )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
     # Include EKF localization launch file
     ekf_navsat = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'dual_ekf_navsat.launch.py'
+            get_package_share_directory(
+                package_name), 'launch', 'dual_ekf_navsat.launch.py'
         )])
     )
 
     # Include the Gazebo launch file
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
+            get_package_share_directory(
+                'gazebo_ros'), 'launch', 'gazebo.launch.py'
         )]),
-        launch_arguments={'world': world_file_path, 'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+        launch_arguments={'world': world_file_path,
+                          'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
     )
 
     # Run the spawner node to spawn the robot in Gazebo
@@ -45,7 +51,7 @@ def generate_launch_description():
                             'robot_description',
                             '-entity',
                             'my_bot'
-                            ],
+                        ],
                         output='screen')
 
     # Spawn differential drive controller
@@ -61,21 +67,23 @@ def generate_launch_description():
         executable="spawner",
         arguments=["joint_broad"],
     )
-    
 
     keyboard = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','keyboard.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(
+                package_name), 'launch', 'keyboard.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
-    nav2_params = os.path.join(get_package_share_directory(package_name), 'config', 'nav2_params.yaml')
+    nav2_params = os.path.join(get_package_share_directory(
+        package_name), 'config', 'nav2_params.yaml')
     configured_params = RewrittenYaml(
         source_file=nav2_params, root_key="", param_rewrites="", convert_types=True
     )
     # Include the Nav2 bringup launch file
     nav2_launch_file = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('nav2_bringup'), 'launch', 'navigation_launch.py'
+            get_package_share_directory(
+                'nav2_bringup'), 'launch', 'navigation_launch.py'
         )]),
         launch_arguments={
             'use_sim_time': 'true',
@@ -84,12 +92,23 @@ def generate_launch_description():
         }.items()
     )
 
+    twist_mux_config = os.path.join(get_package_share_directory(
+        package_name), 'config', 'twist_mux.yaml')
+    twist_mux = Node(
+        package='twist_mux',         # Package name for twist multiplexer
+        executable='twist_mux',      # Executable name
+        name='twist_mux',
+        output='screen',
+        parameters=[twist_mux_config]
+    )
+
     # Return the LaunchDescription
     return LaunchDescription([
         gazebo,
         spawn_entity,
         rsp,
         diff_drive_spawner,
+        twist_mux,
         joint_broad_spawner,
         ekf_navsat,
         nav2_launch_file
